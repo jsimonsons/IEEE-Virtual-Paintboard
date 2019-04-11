fd51bc20199effbe4eb94e0c5981baee0503c784
James Simonson <47487061+jsimonsons@users.noreply.github.com>
2019-04-10 21:04:35 -0700

    Update pin_mux_config.c

--- src/workspace/Sensor_Test/pin_mux_config.c
+++ src/workspace/Sensor_Test/pin_mux_config.c
@@ -152,4 +152,36 @@ void PinMuxConfig(void)
     // Configure PIN_57 for UART0 UART0_RX
     //
     PinTypeUART(PIN_57, PIN_MODE_3);
+
+	// BELOW THIS IS THE PORTION THAT CONFIGURES THE PINS FOR REST_POST_GET
+    // Enable Peripheral Clocks 
+    //
+    MAP_PRCMPeripheralClkEnable(PRCM_GPIOA1, PRCM_RUN_MODE_CLK);
+    MAP_PRCMPeripheralClkEnable(PRCM_GPIOA3, PRCM_RUN_MODE_CLK);
+    MAP_PRCMPeripheralClkEnable(PRCM_UARTA0, PRCM_RUN_MODE_CLK);
+
+    //
+    // Configure PIN_64 for GPIOOutput
+    //
+    MAP_PinTypeGPIO(PIN_64, PIN_MODE_0, false);
+    MAP_GPIODirModeSet(GPIOA1_BASE, 0x2, GPIO_DIR_MODE_OUT);
+
+    //
+    // Configure PIN_02 for GPIOOutput
+    //
+    MAP_PinTypeGPIO(PIN_02, PIN_MODE_0, false);
+    MAP_GPIODirModeSet(GPIOA1_BASE, 0x8, GPIO_DIR_MODE_OUT);
+
+
+
+    //
+    // Configure PIN_55 for UART0 UART0_TX
+    //
+    MAP_PinTypeUART(PIN_55, PIN_MODE_3);
+
+    //
+    // Configure PIN_57 for UART0 UART0_RX
+    //
+    MAP_PinTypeUART(PIN_57, PIN_MODE_3);
+
 }
